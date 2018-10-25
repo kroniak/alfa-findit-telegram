842f438f6d4a01761131fa4f3c1afff1a35362fa
Nikolay Molchanov <me@kroniak.net>
2018-10-25 12:04:52 +0300

    [add] course to export

--- src/Controllers/ExportController.cs
+++ src/Controllers/ExportController.cs
@@ -34,14 +34,14 @@ namespace FindAlfaITBot.Controllers
                 return StatusCode(403);
 
             var sb = new StringBuilder();
-            sb.AppendLine("EMail;Name;Profession;University;Phone;Telegram;");
+            sb.AppendLine("EMail;Name;Profession;University;Course;Phone;Telegram;");
 
             var students = MongoDBHelper.All().Result;
             if (students.Count() > 0)
             {
                 foreach (var stud in students)
                 {
-                    sb.AppendLine($"{stud.EMail};{stud.Name};{stud.Profession};{stud.University};{stud.Phone};{stud.TelegramName};");
+                    sb.AppendLine($"{stud.EMail};{stud.Name};{stud.Profession};{stud.University};{stud.Course};{stud.Phone};{stud.TelegramName};");
                 }
             }
 
